0fd080ac8c011b586ee91555b54b1f03c69fa7d3
LoicV-ha <loic.vernet1@gmail.com>
2024-12-03 18:13:59 +0100

    First
    Test first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const recipesRoutes = require('./recipes');
+const usersRoutes = require('./users');
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.use('/api/recipes', recipesRoutes);
+app.use('/api/users', usersRoutes);
+
+// Gestion des erreurs
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Une erreur interne est survenue.' });
+});
+
+// Lancer le serveur
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
